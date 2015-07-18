86aad26977921c44fb2c445ec4860f1ce48a9f05
arata <contact@materializing.net>
2015-07-18 20:35:16 +0900

    fixed インストール時の初期データ生成を廃止

--- Config/init.php
+++ Config/init.php
@@ -5,28 +5,6 @@
  */
 $this->Plugin->initDb('plugin', 'UserProfileDisplay');
 /**
- * ユーザー情報を元にデータを作成する
- *   ・設定データがないユーザー用のデータのみ作成する
- * 
- */
-	App::uses('User', 'Model');
-	$UserModel = new User();
-	$userDatas = $UserModel->find('list', array('recursive' => -1));
-	if ($userDatas) {
-		CakePlugin::load('UserProfileDisplay');
-		App::uses('UserProfileDisplay', 'UserProfileDisplay.Model');
-		$UserProfileDisplayModel = new UserProfileDisplay();
-		foreach ($userDatas as $key => $user) {
-			$userProfileDisplayData = $UserProfileDisplayModel->findByUserId($key);
-			$savaData = array();
-			if (!$userProfileDisplayData) {
-				$savaData['UserProfileDisplay']['user_id'] = $key;
-				$UserProfileDisplayModel->create($savaData);
-				$UserProfileDisplayModel->save($savaData, false);
-			}
-		}
-	}
-/**
  * 必要フォルダ初期化
  * 
  */
